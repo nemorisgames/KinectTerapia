11d497c0bba7d3f2e4d11e9fe6b565125218b923
nemorisgames <contacto@nemorisgames.com>
2019-01-28 12:00:22 -0300

    fixes added

--- Assets/1_Arknoid/Scripts/Block.cs
+++ Assets/1_Arknoid/Scripts/Block.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Block : MonoBehaviour {
     public int life = 3;
     public int points = 10;
-	// Use this for initialization
-	void Start () {
-		
+    Color[] colorLevels;
+    // Use this for initialization
+    void Start () {
+        colorLevels = new Color[3] { Color.yellow, Color.green, Color.blue };
+        GetComponent<Renderer>().material.SetColor("_EmissionColor", colorLevels[life - 1]);
 	}
 
     public void BlockHit()
@@ -15,7 +17,6 @@ public class Block : MonoBehaviour {
         life--;
         if(life <= 0)
         {
-            GameManager.instance.AddToScore(points);
             Destroy(gameObject);
             print(GameObject.FindGameObjectsWithTag("Block").Length);
             if(GameObject.FindGameObjectsWithTag("Block").Length <= 1)
@@ -23,6 +24,11 @@ public class Block : MonoBehaviour {
                 GameManager.instance.FinishLevel();
             }
         }
+        else
+        {
+            GetComponent<Renderer>().material.SetColor("_EmissionColor", colorLevels[life - 1]);
+        }
+        GameManager.instance.AddToScore(points);
     }
 
     // Update is called once per frame
