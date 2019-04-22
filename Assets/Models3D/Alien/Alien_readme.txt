b34e94d8689609f39f47f0a5a7b3c918295778e2
nemorisgames <contacto@nemorisgames.com>
2019-04-22 12:25:50 -0400

    graphics until level 6

---
+++ Assets/Models3D/Alien/Alien_readme.txt
@@ -0,0 +1,40 @@
+this package include:
+
+Alien Toon animated 3d model
+
+7 Animations
+1 Texture
+1 Prefabs
+1 Scripts
+1 Skybox
+
+1238 quad polygons on this mesh
+1255 vertices on this mesh
+
+Textures
+
+Alien_D.png
+
+Animations list
+
+attack
+death
+hited
+idle
+jumping
+running
+walking
+
+
+Scene Demo
+keys for enable animations
+
+1=idle
+2=walk
+3=run
+4=jump
+5=attack
+6=hited
+7=die
+
+enjoy game age:)
---
+++ Assets/Models3D/Alien/Scripts/Alien.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alien : MonoBehaviour {
+    public Animator alien;
+    private IEnumerator coroutine;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            alien.SetBool("idle", true);
+            alien.SetBool("walking", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            alien.SetBool("walking", true);
+            alien.SetBool("idle", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            alien.SetBool("walking", false);
+            alien.SetBool("running", true);
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            alien.SetBool("running", false);
+            alien.SetBool("jumping", true);
+            alien.SetBool("idle", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha5))
+        {
+            alien.SetBool("jumping", false);
+            alien.SetBool("attack", true);
+            alien.SetBool("idle", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha6))
+        {
+            alien.SetBool("attack", false);
+            alien.SetBool("hited", true);
+            alien.SetBool("idle", false);
+            StartCoroutine("idle");
+            idle();
+        }
+        if (Input.GetKey(KeyCode.Alpha7))
+        {
+            alien.SetBool("hited", false);
+            alien.SetBool("death", true);
+            alien.SetBool("idle", false);
+        }
+    }
+    IEnumerator idle()
+    {
+        yield return new WaitForSeconds(0.1f);
+        alien.SetBool("attack", false);
+        alien.SetBool("hited", false);
+        alien.SetBool("idle", true);
+        alien.SetBool("jumping", false);
+        alien.SetBool("attack", false);
+    }
+}
