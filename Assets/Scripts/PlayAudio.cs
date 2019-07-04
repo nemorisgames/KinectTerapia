515a89c44d9b93c2da13fff5ea58c8adf156448b
njaramillo-ng <njaramillo@nemorisgames.com>
2019-07-04 11:16:20 -0400

    audio bienvenida

---
+++ Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+	public AudioClip[] audios;
+	private AudioSource audioSource;
+
+	void Awake ()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void Start ()
+	{
+		Random.InitState (System.DateTime.Now.Second);
+		audioSource.PlayOneShot (audios[Random.Range (0, audios.Length - 1)]);
+	}
+}
