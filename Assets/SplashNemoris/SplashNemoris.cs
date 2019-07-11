7a882820bb6c4e42dd0a139e21774ac71cd40b45
nemorisgames <contacto@nemorisgames.com>
2019-07-11 10:19:44 -0400

    logos

---
+++ Assets/SplashNemoris/SplashNemoris.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashNemoris : MonoBehaviour {
+    public UnityEngine.Video.VideoClip videoClip;
+    UnityEngine.Video.VideoPlayer videoPlayer;
+    public string nextScene;
+
+    void Start()
+    {
+        videoPlayer = gameObject.AddComponent<UnityEngine.Video.VideoPlayer>();
+        var audioSource = gameObject.AddComponent<AudioSource>();
+
+        videoPlayer.playOnAwake = false;
+        videoPlayer.clip = videoClip;
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.MaterialOverride;
+        videoPlayer.targetMaterialRenderer = GetComponent<Renderer>();
+        videoPlayer.targetMaterialProperty = "_MainTex";
+        videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
+        videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.loopPointReached += Finish;
+        videoPlayer.Play();
+    }
+
+    void Finish(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    void Update()
+    {
+
+    }
+    /*
+    void Start()
+    {
+        // Will attach a VideoPlayer to the main camera.
+        GameObject camera = GameObject.Find("Main Camera");
+
+        // VideoPlayer automatically targets the camera backplane when it is added
+        // to a camera object, no need to change videoPlayer.targetCamera.
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+        // Play on awake defaults to true. Set it to false to avoid the url set
+        // below to auto-start playback since we're in Start().
+        videoPlayer.playOnAwake = false;
+
+        // By default, VideoPlayers added to a camera will use the far plane.
+        // Let's target the near plane instead.
+        videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
+
+        // This will cause our Scene to be visible through the video being played.
+        videoPlayer.targetCameraAlpha = 0.5F;
+
+        // Set the video to play. URL supports local absolute or relative paths.
+        // Here, using absolute.
+        //videoPlayer.url = "/Users/graham/movie.mov";
+        videoPlayer.url = "Assets/SplashNemoris/SplashNemoris.mp4";
+
+        // Skip the first 100 frames.
+        videoPlayer.frame = 100;
+
+        // Restart from beginning when done.
+        videoPlayer.isLooping = true;
+
+        // Each time we reach the end, we slow down the playback by a factor of 10.
+        videoPlayer.loopPointReached += EndReached;
+
+        // Start playback. This means the VideoPlayer may have to prepare (reserve
+        // resources, pre-load a few frames, etc.). To better control the delays
+        // associated with this preparation one can use videoPlayer.Prepare() along with
+        // its prepareCompleted event.
+        videoPlayer.Play();
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+    }*/
+}
