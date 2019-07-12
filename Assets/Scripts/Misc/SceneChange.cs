0cf4b33f13d23f7eac37a83c43b4b9437c8d0f7d
nemorisgames <contacto@nemorisgames.com>
2019-07-11 23:09:19 -0400

    optimizacion

---
+++ Assets/Scripts/Misc/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+    public string nextScene;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void GoToNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/SplashNemoris/SplashNemoris.cs
+++ Assets/SplashNemoris/SplashNemoris.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SplashNemoris : MonoBehaviour {
+    
     public UnityEngine.Video.VideoClip videoClip;
     UnityEngine.Video.VideoPlayer videoPlayer;
     public string nextScene;
@@ -20,7 +21,20 @@ public class SplashNemoris : MonoBehaviour {
         videoPlayer.targetMaterialProperty = "_MainTex";
         videoPlayer.audioOutputMode = UnityEngine.Video.VideoAudioOutputMode.AudioSource;
         videoPlayer.SetTargetAudioSource(0, audioSource);
+        videoPlayer.Prepare();
         videoPlayer.loopPointReached += Finish;
+        videoPlayer.prepareCompleted += Prepared;
+    }
+
+    private void OnDisable()
+    {
+        videoPlayer.loopPointReached -= Finish;
+        videoPlayer.prepareCompleted -= Prepared;
+    }
+
+    void Prepared(UnityEngine.Video.VideoPlayer vp)
+    {
+        print("prepared");
         videoPlayer.Play();
     }
 
@@ -34,6 +48,9 @@ public class SplashNemoris : MonoBehaviour {
 
     }
     /*
+    UnityEngine.Video.VideoPlayer videoPlayer;
+    public string nextScene;
+
     void Start()
     {
         // Will attach a VideoPlayer to the main camera.
@@ -41,7 +58,7 @@ public class SplashNemoris : MonoBehaviour {
 
         // VideoPlayer automatically targets the camera backplane when it is added
         // to a camera object, no need to change videoPlayer.targetCamera.
-        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+        videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
 
         // Play on awake defaults to true. Set it to false to avoid the url set
         // below to auto-start playback since we're in Start().
@@ -52,7 +69,7 @@ public class SplashNemoris : MonoBehaviour {
         videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.CameraNearPlane;
 
         // This will cause our Scene to be visible through the video being played.
-        videoPlayer.targetCameraAlpha = 0.5F;
+        videoPlayer.targetCameraAlpha = 1F;
 
         // Set the video to play. URL supports local absolute or relative paths.
         // Here, using absolute.
@@ -60,10 +77,10 @@ public class SplashNemoris : MonoBehaviour {
         videoPlayer.url = "Assets/SplashNemoris/SplashNemoris.mp4";
 
         // Skip the first 100 frames.
-        videoPlayer.frame = 100;
+        //videoPlayer.frame = 100;
 
         // Restart from beginning when done.
-        videoPlayer.isLooping = true;
+        //videoPlayer.isLooping = true;
 
         // Each time we reach the end, we slow down the playback by a factor of 10.
         videoPlayer.loopPointReached += EndReached;
@@ -73,10 +90,22 @@ public class SplashNemoris : MonoBehaviour {
         // associated with this preparation one can use videoPlayer.Prepare() along with
         // its prepareCompleted event.
         videoPlayer.Play();
+        //videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += Prepared;
+    }
+
+    private void OnDisable()
+    {
+        videoPlayer.prepareCompleted -= Prepared;
+    }
+
+    void Prepared(UnityEngine.Video.VideoPlayer vp)
+    {
+        videoPlayer.Play();
     }
 
     void EndReached(UnityEngine.Video.VideoPlayer vp)
     {
-        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+        SceneManager.LoadScene(nextScene);
     }*/
 }
